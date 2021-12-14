f33361a8a5a2e27d22b8814f9fa8d54bf5d1d0c2
nohaa852 <nohaayman40@gmail.com>
2021-12-15 00:38:23 +0200

    login

--- app/app.js
+++ app/app.js
@@ -1,7 +1,23 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 var express = require('express');
 var path = require('path');
 var app = express();
-var fs = require('fs');
+var bcrypt = require('bcrypt');
+
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const initializePassport = require('./passport.config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -10,6 +26,15 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
 //mongoose connection
 async function main(){
@@ -25,42 +50,62 @@ async function main(){
 main().catch(console.sever);
 
 // users
-var u = fs.readFileSync('users.json');
-var obj = JSON.parse(u);
+// var u = fs.readFileSync('users.json');
+// var obj = JSON.parse(u);
+// console.log(obj.username);
 
-console.log(obj.username);
-
-let data = {
-  "list": [
+let users = [
     {username:"noha","password":"abc"},
     {"username":"shahenda","password":"abc"},
     {"username":"ahmed","password":"abc"}
- ]}
-
- for (var x in obj) {
-   if(obj[x].username =="noha"){
-    console.log('found')
- }
-}
+ ]
 
 app.get('/', function(req, res){
   res.render('index', {tittle: "express"})
 });
 
-app.post('/', function(req, res){
-  
-  var name = req.body.username;
-  var pass = req.body.password;
+app.post('/', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/home',
+  failureRedirect: '/',
+  failureFlash: true
+}))
 
-  if(data.list.find( record => record.username === name)){
-    if(data.list.find( record => record.password === pass)){
-      res.render('home', {tittle: "express"})
-    }
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
   }
-  else{
-    console.log('user not found!');
+  res.redirect('/')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.render('home', {tittle: "express"})
+
   }
+  next()
+}
+
+
+app.get('/register', function(req, res){
+  res.render('register.ejs')
 });
+
+app.post('/register', async (req, res)=> {
+try{
+  const hashedPassword = await bcrypt.hash(req.body.password, 10)
+  users.push({
+    id: Date.now().toString(),
+    name: req.body.name, 
+    email: req.body.email, 
+    password: hashedPassword
+  })
+  res.render('index', {tittle: "express"})
+}catch{
+  res.render('register', {tittle: "express"})
+}
+console.log(users)
+})
+
 
 app.get('/home', function(req, res){
   res.render('home', {tittle: "express"})
@@ -76,7 +121,9 @@ app.post('/reset_pass', function(req, res){
   var name = req.body.username;
   var pass = req.body.new_psw;
 
- 
+  if(data.list.find( record => record.username === name)){
+
+  }
   res.render('index', {tittle: "express"})
   
 });
